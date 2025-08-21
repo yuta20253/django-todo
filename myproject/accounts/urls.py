74d07695f15f79cbd0675d3996301214fff794dc
yuta20253 <magee.20253@gmail.com>
2025-08-22 00:37:07 +0900

    refactor: テンプレートをリファクタリング

--- myproject/accounts/urls.py
+++ myproject/accounts/urls.py
@@ -4,6 +4,6 @@ from .views import CustomLoginView, DashBoardView
 
 urlpatterns = [
     path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(template_name="accounts/logout.html"), name="logout"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
     path("dashboard/", DashBoardView.as_view(), name="dashboard")
 ]
