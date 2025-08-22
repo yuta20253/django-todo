d43a36266348a6bff3e1eeda3eed6e0f37946ed2
yuta20253 <magee.20253@gmail.com>
2025-08-23 04:07:17 +0900

    feat: Todoの各ページにLoginRequiredMixinを適用

--- myproject/todos/views.py
+++ myproject/todos/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Todo
 from .forms import TodoForm
 
 # Create your views here.
-class TodoListView(ListView):
+class TodoListView(LoginRequiredMixin, ListView):
     model = Todo
     template_name = 'todos/index.html'
     context_object_name = 'todos'
 
-class TodoDetailView(DetailView):
+class TodoDetailView(LoginRequiredMixin, DetailView):
     model = Todo
     template_name = 'todos/detail.html'
     context_object_name = 'todo'
 
-class TodoUpdateView(UpdateView):
+class TodoUpdateView(LoginRequiredMixin, UpdateView):
     model = Todo
     form_class = TodoForm
     template_name = 'todos/form.html'
     success_url = reverse_lazy('todo_list')
 
-class TodoDeleteView(DeleteView):
+class TodoDeleteView(LoginRequiredMixin, DeleteView):
     model = Todo
     template_name = 'todos/confirm_delete.html'
     success_url = reverse_lazy('todo_list')
