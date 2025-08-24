a8f3e8eca5fc524945db10724f8097f2d3a8c4eb
yuta20253 <magee.20253@gmail.com>
2025-08-24 23:04:27 +0900

    feat: カテゴリ検索機能追加

--- myproject/todos/views.py
+++ myproject/todos/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.db.models import Q
-from .models import Todo
+from .models import Todo, Category
 from .forms import TodoForm
 
 # Create your views here.
@@ -13,9 +13,19 @@ class TodoListView(LoginRequiredMixin, ListView):
     context_object_name = 'todos'
     def get_queryset(self):
         user = self.request.user
-        return Todo.objects.filter(
+        queryset = Todo.objects.filter(
             Q(user=user) | Q(group__in=user.todo_groups.all())
         )
+        category_id = self.request.GET.get('category')
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        return context
 
 class TodoDetailView(LoginRequiredMixin, DetailView):
     model = Todo
